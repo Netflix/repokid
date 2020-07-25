40afe6e50e8095114af20a273ce02415eb1c313a
Patrick Sanders <psanders@netflix.com>
2020-07-24 17:07:26 -0700

    replace sys.exit() with exceptions, only fetch blocklist once

--- repokid/filters/blocklist/__init__.py
+++ repokid/filters/blocklist/__init__.py
@@ -1,9 +1,9 @@
 import json
 import logging
-import sys
 
 import botocore
 from cloudaux.aws.sts import boto3_cached_conn
+from repokid.exceptions import BlocklistError
 from repokid.filters import Filter
 
 LOGGER = logging.getLogger("repokid")
@@ -27,30 +27,26 @@ def get_blocklist_from_bucket(bucket_config):
         blocklist_json = json.loads(blocklist)
     # Blocklist problems are really bad and we should quit rather than silently continue
     except (botocore.exceptions.ClientError, AttributeError):
-        LOGGER.error(
+        LOGGER.critical(
             "S3 blocklist config was set but unable to connect retrieve object, quitting"
         )
-        sys.exit(1)
+        raise BlocklistError("Could not retrieve blocklist")
     except ValueError:
-        LOGGER.error(
+        LOGGER.critical(
             "S3 blocklist config was set but the returned file is bad, quitting"
         )
-        sys.exit(1)
-    if set(blocklist_json.keys()) != set(["arns", "names"]):
-        LOGGER.error("S3 blocklist file is malformed, quitting")
-        sys.exit(1)
+        raise BlocklistError("Could not parse blocklist")
+    if set(blocklist_json.keys()) != {"arns", "names"}:
+        LOGGER.critical("S3 blocklist file is malformed, quitting")
+        raise BlocklistError("Could not parse blocklist")
     return blocklist_json
 
 
 class BlocklistFilter(Filter):
-    def __init__(self, config=None):
-        blocklist_json = None
-        bucket_config = config.get(
-            "blocklist_bucket", config.get("blacklist_bucket", None)
-        )
-        if bucket_config:
-            blocklist_json = get_blocklist_from_bucket(bucket_config)
+    blocklist_json = None
 
+    def __init__(self, config=None):
+        super().__init__(config)
         current_account = config.get("current_account") or None
         if not current_account:
             LOGGER.error("Unable to get current account for Blocklist Filter")
@@ -63,19 +59,32 @@ class BlocklistFilter(Filter):
             [rolename.lower() for rolename in config.get("all", [])]
         )
 
-        if blocklist_json:
+        if BlocklistFilter.blocklist_json:
             blocklisted_role_names.update(
                 [
                     name.lower()
-                    for name, accounts in blocklist_json["names"].items()
+                    for name, accounts in BlocklistFilter.blocklist_json[
+                        "names"
+                    ].items()
                     if ("all" in accounts or config.get("current_account") in accounts)
                 ]
             )
 
         self.blocklisted_arns = (
-            set() if not blocklist_json else blocklist_json.get("arns", [])
+            set()
+            if not BlocklistFilter.blocklist_json
+            else BlocklistFilter.blocklist_json.get("arns", [])
         )
         self.blocklisted_role_names = blocklisted_role_names
+
+    @classmethod
+    def init_blocklist(cls, config):
+        if not cls.blocklist_json:
+            bucket_config = config.get(
+                "blocklist_bucket", config.get("blacklist_bucket", None)
+            )
+            if bucket_config:
+                cls.blocklist_json = get_blocklist_from_bucket(bucket_config)
 
     def apply(self, input_list):
         blocklisted_roles = []
