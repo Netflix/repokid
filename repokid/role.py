fe6d69834a3f91ff910fcf8ea9724073517da5ae
Patrick Sanders <psanders@netflix.com>
2020-11-13 11:18:14 -0800

    Refactor Role model to use Pydantic

--- repokid/role.py
+++ repokid/role.py
@@ -11,96 +11,80 @@
 #     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
-import copy
+from __future__ import annotations
 
-dict_to_attr = {
-    "AAData": {"attribute": "aa_data", "default": dict()},
-    "Account": {"attribute": "account", "default": None},
-    "Active": {"attribute": "active", "default": True},
-    "Arn": {"attribute": "arn", "default": None},
-    "AssumeRolePolicyDocument": {
-        "attribute": "assume_role_policy_document",
-        "default": None,
-    },
-    "CreateDate": {"attribute": "create_date", "default": None},
-    "DisqualifiedBy": {"attribute": "disqualified_by", "default": list()},
-    "NoRepoPermissions": {"attribute": "no_repo_permissions", "default": dict()},
-    "OptOut": {"attribute": "opt_out", "default": dict()},
-    "Policies": {"attribute": "policies", "default": list()},
-    "Refreshed": {"attribute": "refreshed", "default": str()},
-    "RepoablePermissions": {"attribute": "repoable_permissions", "default": int()},
-    "RepoableServices": {"attribute": "repoable_services", "default": list()},
-    "Repoed": {"attribute": "repoed", "default": str()},
-    "RepoScheduled": {"attribute": "repo_scheduled", "default": int()},
-    "RoleId": {"attribute": "role_id", "default": None},
-    "RoleName": {"attribute": "role_name", "default": None},
-    "ScheduledPerms": {"attribute": "scheduled_perms", "default": dict()},
-    "Stats": {"attribute": "stats", "default": list()},
-    "Tags": {"attribute": "tags", "default": list()},
-    "TotalPermissions": {"attribute": "total_permissions", "default": int()},
-}
+import datetime
+from typing import Dict, List, Optional
+
+from pydantic import BaseModel, Field
 
 
-class Role(object):
-    def __init__(self, role_dict):
-        for key, value in list(dict_to_attr.items()):
-            setattr(
-                self,
-                value["attribute"],
-                role_dict[key] if key in role_dict else copy.copy(value["default"]),
-            )
+class Role(BaseModel):
+    aa_data: Optional[List] = Field(alias="AAData")
+    account: Optional[str] = Field(alias="Account")
+    active: Optional[bool] = Field(alias="Active")
+    arn: Optional[str] = Field(alias="Arn")
+    assume_role_policy_document: Dict = Field(
+        alias="AssumeRolePolicyDocument", default={}
+    )
+    create_date: Optional[datetime.datetime] = Field(alias="CreateDate")
+    disqualified_by: List = Field(alias="DisqualifiedBy", default=[])
+    no_repo_permissions: Dict = Field(alias="NoRepoPermissions", default={})
+    opt_out: Dict = Field(alias="OptOut", default={})
+    policies: List = Field(alias="Policies", default=[])
+    refreshed: Optional[str] = Field(alias="Refreshed")
+    repoable_permissions: Optional[int] = Field(alias="RepoablePermissions")
+    repoable_services: List = Field(alias="RepoableServices", default=[])
+    repoed: Optional[str] = Field(alias="Repoed")
+    repo_scheduled: Optional[int] = Field(alias="RepoScheduled")
+    role_id: Optional[str] = Field(alias="RoleId")
+    role_name: Optional[str] = Field(alias="RoleName")
+    scheduled_perms: List = Field(alias="ScheduledPerms", default=[])
+    stats: List = Field(alias="Stats", default=[])
+    tags: List = Field(alias="Tags", default=[])
+    total_permissions: Optional[int] = Field(alias="TotalPermissions")
 
-    def as_dict(self):
-        return {
-            key: getattr(self, value["attribute"])
-            for key, value in dict_to_attr.items()
-        }
-
-    def set_attributes(self, attributes_dict):
-        for key, value in attributes_dict.items():
-            setattr(self, dict_to_attr[key]["attribute"], value)
-
-    def __eq__(self, other):
+    def __eq__(self, other: str) -> bool:
         return self.role_id == other
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash(self.role_id)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.role_id
 
 
 class Roles(object):
-    def __init__(self, role_object_list):
-        self.roles = role_object_list
+    def __init__(self, role_object_list: List[Role]):
+        self.roles: List[Role] = role_object_list
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> Role:
         return self.roles[index]
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.roles)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str([role.role_id for role in self.roles])
 
-    def __eq__(self, other):
+    def __eq__(self, other: Roles) -> bool:
         return all(role.role_id in other for role in self.roles) and all(
             role.role_id in self.roles for role in other
         )
 
-    def append(self, role):
+    def append(self, role: Role):
         self.roles.append(role)
 
-    def role_id_list(self):
+    def role_id_list(self) -> List[str]:
         return [role.role_id for role in self.roles]
 
-    def get_by_id(self, id):
+    def get_by_id(self, id) -> Optional[Role]:
         try:
             return self.filter(role_id=id)[0]
         except IndexError:
             return None
 
-    def filter(self, **kwargs):
+    def filter(self, **kwargs) -> List[Role]:
         roles = self.roles
         for arg, value in kwargs.items():
             roles = [role for role in roles if getattr(role, arg, None) == value]
