0cdfe390097734398cd3ea9f97a0a8f11499f0c1
Patrick Sanders <psanders@netflix.com>
2020-11-19 15:54:58 -0800

    Use setupmeta for versioning

--- repokid/__init__.py
+++ repokid/__init__.py
@@ -20,8 +20,6 @@ import os
 
 import import_string
 
-__version__ = "0.14.8"
-
 
 def init_config():
     """
--- repokid/cli/repokid_cli.py
+++ repokid/cli/repokid_cli.py
@@ -40,7 +40,6 @@ import logging
 import sys
 
 from docopt import docopt
-from repokid import __version__ as __version__
 from repokid import CONFIG
 from repokid import get_hooks
 from repokid.commands.repo import (
@@ -62,6 +61,7 @@ from repokid.commands.schedule import (
     _show_scheduled_roles,
 )
 from repokid.utils.dynamo import dynamo_get_or_create_table
+import runez
 
 
 # http://docs.aws.amazon.com/IAM/latest/UserGuide/reference_iam-limits.html
@@ -187,7 +187,7 @@ def _generate_default_config(filename=None):
 
 
 def main():
-    args = docopt(__doc__, version="Repokid {version}".format(version=__version__))
+    args = docopt(__doc__, version=f"Repokid {runez.get_version(__name__)}")
 
     if args.get("config"):
         config_filename = args.get("<config_filename>")
