ca2c0e041d9410f1984256ebf8a473bb3b741ca4
Patrick Sanders <psanders@netflix.com>
2020-11-24 14:50:11 -0800

    it's always the import order

--- repokid/cli/repokid_cli.py
+++ repokid/cli/repokid_cli.py
@@ -12,9 +12,10 @@
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 
-import click
 import json
 import logging
+
+import click
 
 from repokid import CONFIG
 from repokid import get_hooks
