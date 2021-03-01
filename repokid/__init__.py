0a10ac2938d301579560fd75e9917e72bfc621f6
Patrick Sanders <psanders@netflix.com>
2021-03-01 15:40:31 -0800

    Version 0.17.10

--- repokid/__init__.py
+++ repokid/__init__.py
@@ -27,7 +27,7 @@ from repokid.types import RepokidConfig
 from repokid.types import RepokidHook
 from repokid.types import RepokidHooks
 
-__version__ = "0.17.9"
+__version__ = "0.17.10"
 
 
 def init_config() -> RepokidConfig:
